33cc903fd999e043ee9ac20faa7346bd81de1947
Erik <erikwjonsson@gmail.com>
2018-09-13 08:01:24 +0200

    Removed unnecessary view. Made welcome default view with instructions.

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-
-Route::get('/', function () {
-    return view('programme_finder');
-});
-
-//Route::resource('programmes', 'ProgrammeController');
 
 // Add CSRF protection later. https://laravelcollective.com/docs/5.0/html#csrf-protection
 Route::get('programmes/show', 'ProgrammeController@show')->name('show');
